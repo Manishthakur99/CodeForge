4737ddbbd45830df146e3351b7afaab38954c7c5
Manish Thakur <thakurify@gmail.com>
2026-03-30 15:51:08 +0530

    Add razorpay key debug logs

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,5 +1,7 @@
 const Razorpay = require("razorpay");
 
+console.log("Razorpay Key:", process.env.RAZORPAY_KEY ? "Found" : "MISSING");
+console.log("Razorpay Secret:", process.env.RAZORPAY_SECRET ? "Found" : "MISSING");
 
 exports.instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY,
